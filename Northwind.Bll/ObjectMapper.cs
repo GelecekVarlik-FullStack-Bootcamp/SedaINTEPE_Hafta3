769a2ef64e4a22010d41be66bf9a444e38c2a98d
Seda <sedaintepe995@gmail.com>
2022-04-09 15:34:59 +0300

    İskeletin olusturulması.,

---
+++ Northwind.Bll/ObjectMapper.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Northwind.Entity.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Bll
+{
+    internal class ObjectMapper //sadece Bll de olsun.Kalıtım vermeyeceğim için protected , nesen turetme yontemi ile yaptığımız için internal kullanırız
+    {
+        //Lazy sadece iihtiyacımız oldugunda gel demek.
+        static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(()=> //bu işaret (=>) oyleki demek
+          {
+              var config = new MapperConfiguration(cfg =>
+                {
+                    //Birden cok mappingprofile varsa altına ekleriz
+                    cfg.AddProfile<MappingProfile>();//Add profile mappingteki mappingprofile demek
+                });
+              return config.CreateMapper();
+
+          } 
+        );
+        public static IMapper Mapper => lazy.Value;
+
+    }
+}
---
+++ Northwind.Entity/IBase/IResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entity.IBase
+{
+    interface IResponse//geri dondur
+    {
+    }
+    interface IResponse<T> //generic yaptık
+    {
+    }
+}
