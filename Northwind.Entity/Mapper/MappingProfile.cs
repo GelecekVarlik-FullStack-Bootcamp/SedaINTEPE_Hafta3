821de1746f52e74bc737011e646ad5679804c2b3
Seda <sedaintepe995@gmail.com>
2022-04-12 07:18:38 +0300

    Mapping

--- Northwind.Entity/Mapper/MappingProfile.cs
+++ Northwind.Entity/Mapper/MappingProfile.cs
@@ -13,7 +13,36 @@ namespace Northwind.Entity.Mapper
     {
         public MappingProfile()
         {
-            CreateMap<Customer, DtoCustomer>().ReverseMap();//reverse map sırası için onemli.yazarsak sırasını istediğii gibi yapabilirz. yoksa kaynak, hedef olmalı.
+            CreateMap<AlphabeticalListOfProduct, DtoAlphabeticalListOfProduct>();
+            CreateMap<Category, DtoCategory>();
+            CreateMap<CategorySalesFor1997, DtoCategorySalesFor1997>();
+            CreateMap<CurrentProductList, DtoCurrentProductList>();
+            CreateMap<Customer, DtoCustomer>();// ReverseMap();//reverse map sırası için onemli.yazarsak sırasını istediğii gibi yapabilirz. yoksa kaynak, hedef olmalı.
+            CreateMap<CustomerAndSuppliersByCity, DtoCustomerAndSuppliersByCity>();
+            CreateMap<CustomerCustomerDemo, DtoCustomerCustomerDemo>();
+            CreateMap<CustomerDemographic, DtoCustomerDemographic>();
+            CreateMap<DtoEmployee, Employee>().ReverseMap();
+            CreateMap<EmployeeTerritory, DtoEmployeeTerritory>();
+            CreateMap<Invoice, DtoInvoice>();
+            CreateMap<Order, DtoOrder>();
+            CreateMap<OrderDetail, DtoOrderDetail>();
+            CreateMap<OrderDetailsExtended, DtoOrderDetailsExtended>();
+            CreateMap<OrdersQry, DtoOrdersQry>();
+            CreateMap<OrderSubtotal, DtoOrderSubtotal>();
+            CreateMap<Product, DtoProduct>();
+            CreateMap<ProductsAboveAveragePrice, DtoProductsAboveAveragePrice>();
+            CreateMap<ProductSalesFor1997, DtoProductSalesFor1997>();
+            CreateMap<ProductsByCategory, DtoProductsByCategory>();
+            CreateMap<QuarterlyOrder, DtoQuarterlyOrder>();
+            CreateMap<Region, DtoRegion>();
+            CreateMap<SalesByCategory, DtoSalesByCategory>();
+            CreateMap<SalesTotalsByAmount, DtoSalesTotalsByAmount>();
+            CreateMap<Shipper, DtoShipper>();
+            CreateMap<SummaryOfSalesByQuarter, DtoSummaryOfSalesByQuarter>();
+            CreateMap<SummaryOfSalesByYear, DtoSummaryOfSalesByYear>();
+            CreateMap<Supplier, DtoSupplier>();
+            CreateMap<Territory, DtoTerritory>();
+            CreateMap<User, DtoUser>();
         }
     }
 }
